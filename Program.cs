68a0580c7486af3d1b2bb1c18b50a7a658308a5f
Victor Bouffier <victor@logicamente.mx>
2021-12-17 10:54:07 -0600

    Added CurrentDirectory fn into search, deleted duplicate project directory

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
-var salesFiles = FindFiles("stores");
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
 
 foreach (var file in salesFiles)
 {
-    System.Console.WriteLine(file);
+    Console.WriteLine(file);
 }
 
-Console.WriteLine(Directory.GetCurrentDirectory());
-System.Console.WriteLine(Environment.GetFolderPath());
-
+// Console.WriteLine("CurrentDirectory: " + Directory.GetCurrentDirectory());
+// Console.WriteLine("ApplicationData: " + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+// Console.WriteLine("MyDocuments: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+// Console.WriteLine("MyMusic: " + Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+// Console.WriteLine("Desktop: " + Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+// Console.WriteLine();
+// string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+// Console.WriteLine(fileName);
+// FileInfo info = new FileInfo(fileName);
+// Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
 // ----------- END OF MAIN ------------
 
 // FindFiles method
@@ -18,7 +28,7 @@ IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
 
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    var foundFiles = Directory.EnumerateFiles(currentDirectory, "*", SearchOption.AllDirectories);
 
     foreach (var file in foundFiles)
     {
@@ -31,3 +41,4 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
