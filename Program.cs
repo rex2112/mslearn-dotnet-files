96a9ba99b185d7957bc2ed3faded14acc31af9aa
Victor Bouffier <victor@logicamente.mx>
2021-12-17 16:07:24 -0600

    writing totals to totals.txt from json files

--- Program.cs
+++ Program.cs
@@ -28,12 +28,13 @@ IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
 
-    var foundFiles = Directory.EnumerateFiles(currentDirectory, "*", SearchOption.AllDirectories);
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
     foreach (var file in foundFiles)
     {
         // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json" || extension == ".txt")
         {
             salesFiles.Add(file);
         }
